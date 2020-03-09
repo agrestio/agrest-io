3b56e9576a9f30372131fe6fb56c1ced848f3125
Nikita Timofeev <stariy95@gmail.com>
2020-03-09 16:48:29 +0300

    Update documentation build script

--- build-asciidocs.sh
+++ build-asciidocs.sh
@@ -16,12 +16,11 @@ function clearDir() {
 
 # no input, so just exit
 if [ -z "$1" ]; then
-    echo "Usage: build-asciidocs.sh git-tag [agrest]"
+    echo "Usage: build-asciidocs.sh git-tag"
     exit -1
 fi
 
 GIT_TAG="$1"
-#VERSION="$GIT_TAG"
 
 # version can be passed as a second parameter
 if [ -z "$2" ]; then
@@ -37,34 +36,30 @@ BASE_DIR=`pwd` # base project dir
 echo "Working dir: $BASE_DIR"
 
 # init and check paths
-#MAJOR_VERSION="${VERSION:0:3}" # expecting version format as X.Y.something-else, i.e. 4.1.M1 is ok, but 4.12.M1 will fail..
 ASCII_DOC_DIR="$BASE_DIR/content/page/docs"     # Asciidoc goes to content, Hugo will process it
-AGREST_TMP_DIR="$BASE_DIR/target/tmp"                          # tmp directory to checkout Cayenne
+AGREST_TMP_DIR="$BASE_DIR/target/tmp"           # tmp directory to checkout Agrest
 
 # prepare all directories
 clearDir          "$AGREST_TMP_DIR"
 checkAndCreateDir "$ASCII_DOC_DIR"
 
-#echo "Building docs for Cayenne $MAJOR_VERSION ($VERSION)"
-
 # clone git repo and checkout requested TAG
-git clone https://github.com/KravchenkoAS/agrest.git "$AGREST_TMP_DIR" --branch "$GIT_TAG" --depth 1
+git clone https://github.com/agrestio/agrest.git "$AGREST_TMP_DIR" --branch "$GIT_TAG" --depth 1
 # we will need Maven to build only asciidoc modules
 cd  "$AGREST_TMP_DIR/agrest-docs/"
 
 # build it
 echo "Running Maven build... it can take a while..."
-mvn install -Passembly -DskipTests
-#mvn install -Passembly -DskipTests -Dcayenne.version=${VERSION} > /dev/null 2>&1
+mvn package -DskipTests
 echo "Maven build complete"
 
 # copy everything from ./docs/asciidoc/**/target/site/** directories
 for d in */ ; do
     # skip asciidoc extension module
-#    if [ "$d" == "cayenne-asciidoc-extension/" ]; then
-#        continue
-#    fi
+    if [ "$d" == "agrest-asciidoc-postprocessor/" ]; then
+        continue
+    fi
 
     echo "Syncing asciidoc content for ${d}"
-    cp -R "./${d}target/tmp/." "$ASCII_DOC_DIR/"
+    cp -R "./${d}target/site/." "$ASCII_DOC_DIR/"
 done
