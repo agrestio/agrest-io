b77b143e60afc148c7094eba1912d956fe631001
Oleg Khodokevich <oleg.khodokevich@mail.ru>
2022-03-04 13:01:16 +0300

    Add version for script

--- build-asciidocs.sh
+++ build-asciidocs.sh
@@ -18,10 +18,11 @@ function clearDir() {
 # get script arguments
 set -x
 args="$@"
+echo $args
 
 # no input, so just exit
 if [ ! -n "$args" ]; then
-    echo "Usage: build-asciidocs.sh git-tags [version]"
+    echo "Usage: build-asciidocs.sh git-branch-1 git-branch-2"
     exit 1
 fi
 
@@ -36,6 +37,7 @@ AGREST_TMP_DIR="$BASE_DIR/target/tmp"           # tmp directory to checkout Agre
 
 # prepare all directories
 clearDir          "$AGREST_TMP_DIR"
+clearDir          "$ASCII_DOC_DIR"
 checkAndCreateDir "$ASCII_DOC_DIR"
 
 for git_tag in ${args[@]}
@@ -43,15 +45,15 @@ do
 	# clone git repo and checkout requested TAG
   AGREST_TMP_DIR_VERSION="$BASE_DIR/target/tmp/$git_tag"           # tmp directory with defined version to checkout Agrest
   GIT_TAG="$git_tag"
-  git clone https://github.com/agrestio/agrest-docs.git "AGREST_TMP_DIR_VERSION" --branch "$GIT_TAG"
-  cd  "AGREST_TMP_DIR_VERSION/" || exit 2
+  git clone https://github.com/agrestio/agrest-docs.git "$AGREST_TMP_DIR_VERSION" --branch "$GIT_TAG" || ( echo "Wrong branch : $GIT_TAG" && exit 2)
+  cd  "$AGREST_TMP_DIR_VERSION/" || exit 2
 
   # build it
   echo "Running Maven build... it can take a while..."
   mvn package -q -B -DskipTests > /dev/null 2>&1
   echo "Maven build complete"
 
-  # copy everything from ./docs/asciidoc/**/target/site/** directories
+  # copy everything from ./target/tmp/**/target/site/** directories
   for d in */ ; do
       # skip asciidoc extension module
       if [ "$d" == "agrest-asciidoc-postprocessor/" ]; then
@@ -60,16 +62,16 @@ do
 
       echo "Syncing asciidoc content for ${d}"
       ASCII_DOC_DIR_VERSION="$ASCII_DOC_DIR/$GIT_TAG"
-      cp -R "./${d}target/site/$GIT_TAG." "$ASCII_DOC_DIR_VERSION/"
+      cp -R "./${d}target/site/." "$ASCII_DOC_DIR_VERSION/"
   done
 
   # create _index.html for list.html
   cd $ASCII_DOC_DIR_VERSION || exit 2
-  echo "---/n---" > _index.html
+  echo $'---\n---' > _index.html
 done
 
 # create _index.html for list.html
 cd $ASCII_DOC_DIR || exit 2
-echo "---/n---" > _index.html
+echo $'---\n---' > _index.html
 
 
